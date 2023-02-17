f73ca27e16f2be4696411362ae4fb96115ade6c9
Huuquy-HAu <quyhuu19@gmail.com>
2023-02-17 16:59:57 +0700

    Quy

--- controller/chatController.js
+++ controller/chatController.js
@@ -4,7 +4,7 @@ exports.getAllChatForRoom = async (req,res) => {
     try {
         const data = await ChatModel.find({
             idRoomChat: req.params.idRoom
-        })
+        }).sort({time : 1})
         if(!data){
             return res.status(400).json('error')
         }
@@ -20,7 +20,6 @@ exports.createOneChat = async (req,res) => {
         const data = await ChatModel.create({
             idRoomChat: req.body.idRoomChat,
             content: req.body.content,
-            time : req.body.time,
             sender: req.body.sender
         })
 
@@ -50,6 +49,33 @@ exports.deleteOneChat = async (req, res) => {
     }
 }
 
+exports.hiddenUser = async (req,res) => {
+    try {
+        const data = await ChatModel.find({
+            idRoomChat: req.params.idRoomChat
+        })
+
+        if(!data) {
+            res.status(400).json('Khong tim thay RoomChat')
+        }
+
+        // data.hidden.push(req.params.idUser)
+
+        // const data2 = await data.save()
+        const data2 = await ChatModel.findOneAndUpdate(
+            { idRoomChat: req.params.idRoomChat }, // Tìm tài liệu cần cập nhật
+            { $addToSet: { hidden: req.params.idUser } }, // Thêm mới ID vào mảng nếu chưa tồn tại
+            { upsert: true, new: true } // Lựa chọn upsert sẽ tạo một tài liệu mới nếu không tìm thấy, new sẽ trả về tài liệu mới sau khi cập nhật
+        )
+        
+        if(!data2) {
+            res.status(400).json('Khong thanh cong')
+        }
+        res.status(200).json('Thanh Cong')
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 
 
 
